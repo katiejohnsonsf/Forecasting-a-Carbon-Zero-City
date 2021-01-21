cc0917387fefad55a3e5dd2e2fa65f9996f3645d
katiejohnsonsf <katiejohnsonsf@gmail.com>
2021-01-20 18:11:20 -0800

    add v1 conf int in Dash app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 
 import pandas as pd
 
@@ -35,6 +36,14 @@ def update_figure(selected_phase):
 
     fig = px.line(filtered_df, x="Date", y="kwh avg",
                   hover_name="kwh avg")
+
+    fig.add_trace(go.Scatter(
+        x=df_ts.Date, 
+        y=df_ts.lower_ci + df_ts.upper_ci,
+        fill='toself',
+        fillcolor='rgba(0,100,80,0.2)',
+        line_color='rgba(255,255,255,0)',
+    ))
 
     fig.update_layout(transition_duration=500, 
                       title='Energy Efficiency Improvements Jan 2020-Jan 2022 (5,384 / 100,313 properties)')
