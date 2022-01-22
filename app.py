1bbc4fc249c4952569c2fb2958131546b7ea7ce2
katiejohnsonsf <katiejohnsonsf@gmail.com>
2022-01-22 12:43:23 -0800

    add data context for plotly app website

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import plotly.graph_objects as go
 
 import pandas as pd
 
-df_ts = pd.read_csv('stationary-data/all_ci_df.csv')
-df_ro = pd.read_csv('stationary-data/preds_min_df.csv')
+df_ts = pd.read_csv('/home/katiejohnsonsf/mysite/stationary-data/all_ci_df.csv')
+df_ro = pd.read_csv('/home/katiejohnsonsf/mysite/stationary-data/preds_min_df.csv')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -17,7 +17,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.H1(children='Analytics for a Carbon Zero City'),
     dcc.Graph(id='graph-with-slider', style={'marginLeft': 10, 'marginRight': 10, 'marginTop': 10, 'marginBottom': 10, }),
-    dcc.Slider(
+    html.Div(dcc.Slider(
         id='phase-slider',
         min=df_ts['phase'].min(),
         max=df_ts['phase'].max(),
@@ -30,8 +30,25 @@ app.layout = html.Div([
             4: {'label': '5,384 properties'}
         },
         step=None
-    )
-], style={'marginLeft': '50px', 'marginTop': '50px', 'marginRight': '50px'})
+    ), style={'marginLeft': 10, 'marginRight': 10, 'marginTop': 10, 'marginBottom': 50, }),
+    dcc.Markdown('''
+#### Intended Audience
+
+This dashboard helps people in the energy efficiency field prioritize energy efficiency projects for maximum impact and allows them to see how their work is impacting monthly average kWh per capita. It allows program managers at local non-profits, utilities, and energy efficiency retrofitting companies to size markets and prioritize reduction opportunities for a city. It helps answer the questions:
+> 1. What is the optimal approach for addressing energy efficiency opportunities in a city?
+> 2. To what extent will those energy efficiency improvements help a city get closer to its Carbon Zero goal?
+
+
+#### Story
+
+Energy efficiency improvements for grid electrical consumption is a core strategy in Gainesville’s Climate Action Plan for achieving carbon zero goal by 2035. This dashboard aligns with Gainesville’s long-term carbon zero goal by tracking how 5,384 energy efficiency improvements impact the city's KPI for grid-electrical emissions, *monthly average kWh per capita* (see y-axis above). The property reductions impacted in the two-year reporting cycle make up ~5% of all buildings in Gainesville, but their 100% improvement reduces grid electrical consumption by 34% between January 2020 and January 2023. Moving the slider from left to right shows the reductions impact of these projects as they are completed. Planning over a two-year period allows people to contribute more actively to Gainesville’s Climate Action Plan 2-year reporting cycle, which allows the city to track its overall progress.
+
+Throughout each two-year reporting cycle, the data supports action and better decision-making by identifying the best energy efficiency opportunities in a city to tackle first. The energy efficiency projects are prioritized based on the difference between the predicted and actual ICC building code release used in a building’s construction. If a property is predicted to have used the 1990 building code but is recorded as having used the 1996 code release, there is a higher likelihood of more significant reductions. This prioritization method allows all properties in the dataset to be ranked from highest to lowest by reduction opportunity. This prioritization reduces more emissions for the city and has increased financial benefits for residents and energy efficiency companies.
+
+Project by [Katie Johnson](https://www.linkedin.com/in/hellokatiejohnson/). More information on data sources and modeling methods are available on Github [here](https://github.com/katiejohnsonsf/Forecasting-a-Carbon-Zero-City).
+
+''')
+], style={'marginLeft': '75px', 'marginTop': '75px', 'marginRight': '75px', 'marginBottom': '75px'})
 
 
 @app.callback(
@@ -48,7 +65,7 @@ def update_figure(selected_phase):
                   hover_name="kwh avg")
 
     fig.add_trace(go.Scatter(
-        x=x+x[::-1], 
+        x=x+x[::-1],
         y=y_upper+y_lower[::-1],
         fill='toself',
         fillcolor='rgba(0,100,80,0.2)',
@@ -57,7 +74,7 @@ def update_figure(selected_phase):
 
     fig.update_layout(yaxis_range=[0,2200],
                       showlegend=False,
-                      transition_duration=500, 
+                      transition_duration=500,
                       title='Impact of Energy Efficiency Improvements in 5,384 / 100,313 properties in Gainesville, FL')
 
     return fig
